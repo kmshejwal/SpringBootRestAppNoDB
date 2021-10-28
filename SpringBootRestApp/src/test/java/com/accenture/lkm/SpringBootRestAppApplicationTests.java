8a97d50c58fa90595b4afb34de274b088f9e7359
kmshejwal <kmshejwal@gmail.com>
2021-10-28 17:00:54 +0530

    Update app to demonstrate build triggers

--- SpringBootRestApp/src/test/java/com/accenture/lkm/SpringBootRestAppApplicationTests.java
+++ SpringBootRestApp/src/test/java/com/accenture/lkm/SpringBootRestAppApplicationTests.java
@@ -2,12 +2,16 @@ package com.accenture.lkm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.logging.Logger;
 
 @SpringBootTest
 class SpringBootRestAppApplicationTests {
 
+	Logger logger= Logger.getLogger("SpringBootRestAppApplicationTests");
+	
 	@Test
 	void contextLoads() {
+		logger.info("in contextLoads() test method");
 	}
 
 }
