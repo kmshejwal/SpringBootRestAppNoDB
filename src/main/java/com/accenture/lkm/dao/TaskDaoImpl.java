c74d52e3e53ba7367e69c28f713a798d2463e0c3
kmshejwal <kmshejwal@gmail.com>
2023-06-22 16:42:44 +0530

    22nd june live session

--- src/main/java/com/accenture/lkm/dao/TaskDaoImpl.java
+++ src/main/java/com/accenture/lkm/dao/TaskDaoImpl.java
@@ -19,7 +19,7 @@ public class TaskDaoImpl implements TaskDao {
 		tasks.put(1002, new TaskBean(1002,"Update priorities","In progress"));
 		tasks.put(1003, new TaskBean(1003,"Set up call with clients","New"));
 		tasks.put(1004, new TaskBean(1004,"Byte sized session demo","In progress"));
-		tasks.put(1005, new TaskBean(1005,"Update activity tracker","In progress"));
+		tasks.put(1005, new TaskBean(1005,"Update activity tracker","Completed"));
 		lastId= 1005;
 	}
 	
